e3215ca8fefc703f3031bd1774af99ce067a975c
TatianaSherstyuk2022 <grinkevichtatiana@gmail.com>
2022-12-29 14:54:44 +0200

    add #1,2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
-// Add imports above this line
+// Add imports above this linen
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const container = document.querySelector(".js-gallery");
+// const galleryItemEl = document.querySelector('.gallery__link')
+
+function createMarkup(arr) {
+  const markup = arr
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        `;
+    })
+    .join("");
+  return markup;
+}
+
+container.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: "250",
+});
+
